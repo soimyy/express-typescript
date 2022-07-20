81839e08cdd8b9aebb13503c78e45ddba532fc0d
soimyy <dayanbiefu7@gmail.com>
2022-07-21 00:16:13 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
